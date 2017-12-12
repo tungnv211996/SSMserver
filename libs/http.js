cbc832e82fe7dbba4ac6d51aef3190c6f2b76918
ivannguyendev <ivannguyen.it@gmail.com>
2017-12-12 06:16:45 +0100

    [update] format coap

--- libs/http.js
+++ libs/http.js
@@ -7,7 +7,11 @@ var check = false;
             if(error || !body){
 console.log('error:', error); // Print the error if one occurred
 res.status(404).end(JSON.stringify({code : '404' , status: 'Sensor not exsited'}));
-} else if(!check && body){res.end(body); check = true;} 
+} else if(!check && body){
+let array = body.split('\n')[2]
+array = array.replace('</body></html>','')
+res.end(array); 
+check = true;} 
         });
 setTimeout(function(){
 	if(!check) {
