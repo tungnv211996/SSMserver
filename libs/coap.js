fb7debd885e7c31d338ab42fbb6bb45cf31548aa
ivannguyendev <ivannguyen.it@gmail.com>
2017-12-12 06:07:10 +0100

    [update] fix sensor coap

--- libs/coap.js
+++ libs/coap.js
@@ -1,28 +1,29 @@
 var coap = require('coap'),
     bl = require('bl')
 
-
 module.exports = {
     getData: function (req, res, ip) {
-var check = false;
-        var server = coap.createServer(function (req, result) {
-            result.end('Hello ' + json + '\n')
+        var server = coap.createServer(function (req, res) {
+            res.end('Hello ' + json + '\n')
         }).listen(function () {
             var req = coap.request('coap://[' + ip + ']:5683/sensors/light')
+var check = false;
             req.on('response', function (re) {
-if(!check && re.payload){
-res.end(re.payload)
-check = true;
-} else res.status(404).end(JSON.stringify({code : '404' , status: 'Sensor not exsited'}));
+                if (!check && re.payload) {
+                    res.end(re.payload)
+                    check = true;
+                } else res.status(404).end(JSON.stringify({ code: '404', status: 'Sensor not exsited' }));
                 res.pipe(bl(function (err, data) {
                 }))
+		//console.log(re);
             })
-            setTimeout(function(){
-	if(!check) {
-res.status(404).end(JSON.stringify({code : '404' , status: 'Sensor not exsited'}));
-	check = true;
-}
-},5000)
+            req.end()
+setTimeout(function () {
+                if (!check) {
+                    res.status(404).end(JSON.stringify({ code: '404', status: 'Sensor not exsited' }));
+                    check = true;
+                }
+            }, 5000)
         })
     }
 }
